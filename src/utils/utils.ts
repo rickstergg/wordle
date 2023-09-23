258c3b491fca0f8b356a0c750f7a96fbefc2bb6a
Rick Zhang <rickzhang@live.ca>
2023-09-23 02:55:55 -0400

    Add isLetter, isDelete, and isEnter utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,15 @@
 export const getRandomInt = (max: number) => {
   return Math.floor(Math.random() * max);
 }
+
+export const isLetter = (keyCode: number) => {
+  return (keyCode >= 65 && keyCode <= 90) || (keyCode >= 97 && keyCode <= 122) ? true : false
+}
+
+export const isDelete = (keyCode: number) => {
+  return keyCode === 8;
+}
+
+export const isEnter = (keyCode: number) => {
+  return keyCode === 13;
+}
